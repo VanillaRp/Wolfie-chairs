40b9e7d13647b12fff6e350fc55feb8f68e5d454
WolfieValeo <89059421+WolfieValeo@users.noreply.github.com>
2021-09-01 23:33:05 -0400

    updates

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
-Config["UseCommands"] = true --- Ability to use commands like /spawnchair1 for everyone
-Config["UseTriggers"] = true -- I recommend having this on it allows for you to use the documentation below to customize this and use it anyway you would like.
+Config["UseCommands"] = false --- Ability to use commands like /spawnchair1 for everyone
+Config["UseTriggers"] = true 
 Config["ClientTrigger1"] = "wolfie-chairs:Chair1"
 Config["ClientTrigger2"] = "wolfie-chairs:Chair2"
 Config["Chair1Command"] = "spawnchair1"
